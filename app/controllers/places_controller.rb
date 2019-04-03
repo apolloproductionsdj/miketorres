1e8c152ab25ac1aa2e61deb55b2e5397d2d36fcd
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-03 21:33:17 +0000

    changed @tasks to @places. NoMethodError coming up

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,9 @@
 class PlacesController < ApplicationController
 
-  def index
-    @places = Place.all
-  end 
+  
+
+def index
+   @places = Place.order(:id).paginate(:page => params[:page], :per_page => 5)
+end
+
 end
