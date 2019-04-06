aa176ea26f84454fe14caa89cddffd5da3c9001d
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-06 16:37:22 +0000

    added page to view places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,13 +10,19 @@ class PlacesController < ApplicationController
   end
 
   def create
-  current_user.places.create(place_params)
-  redirect_to root_path
-end
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
 
   private
 
   def place_params
     params.require(:place).permit(:name, :address, :description)
   end
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,4 @@ class User < ApplicationRecord
 
   has_many :places
 end
+    
