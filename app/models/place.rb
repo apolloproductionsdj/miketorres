e7fd17e70e98c781d30755e1c59dfa9248ae7dfb
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-08 07:05:33 +0000

    2nd attempt

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
   belongs_to :user
-  validates :name, length: {minimum: 3} presence: true
+  validates :name, presence: true length: {minimum: 3} 
   validates :address, presence: true
   validates :description, presence: true
 end
