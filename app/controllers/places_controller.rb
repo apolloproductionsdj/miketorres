8d141ee2faf7939a792dedcec02e3bd4e00d0ae1
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-08 06:44:21 +0000

    changed back validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,18 +10,14 @@ class PlacesController < ApplicationController
   end
 
   def create
-     @place = current_user.places.create(place_params)
-     if @place.valid?
-       redirect_to root_path
-     else
-       render :new, status: :unprocessable_entity
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
-     @address = current_user.addresses.create
-     if @address.valid?
-      redirect_to root_path
-     else
-       render :new, status: :unprocessable_entity
-  end
+end
+  
 
   def show
     @place = Place.find(params[:id])
