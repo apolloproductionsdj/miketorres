c4cee9bccb2a4f8ff694ead4b0936d4a5f3f1aa8
Mike Torres <apolloproductionsdj@gmail.com>
2019-04-14 15:41:54 +0000

    User Profile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :places do
     resources :photos, only: :create
     resources :comments, only: :create
+
   end
+  resources :users, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
